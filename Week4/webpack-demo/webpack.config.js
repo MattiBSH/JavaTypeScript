455a02dade986c45af6284c6b1a743b5eb9a1b81
MattiBSH <cph-mh829@cphbusiness.dk>
2021-02-22 19:54:27 +0100

    m

---
+++ Week4/webpack-demo/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+      },
+      plugins: [
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+
+
+        new HtmlWebpackPlugin({
+          title: 'Output Management',
+        }),
+      ],
+    
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
